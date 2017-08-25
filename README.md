74098e8c91d143bf8bfac186d1196ae6123d8543
MatthieuJnon <mattjoannon@gmail.com>
2017-08-25 14:18:37 +0200

    Extension now shows current number of tabs opened

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # tab-usage-analyzer
 A tool to analyze how you use tabs in your browser and to present the data in a beautiful way.
+
+#Chrome extension :
+
+To add the extension to your chrome installation, just drop the chrome-extension folder to chrome://extensions
---
+++ chrome-extension/main.js
@@ -0,0 +1,28 @@
+
+
+function getNumberOfTabs(callback) {
+  // Query filter to be passed to chrome.tabs.query - see
+  // https://developer.chrome.com/extensions/tabs#method-query
+  var queryInfo = {
+    currentWindow: true
+  };
+
+  chrome.tabs.query(queryInfo, (tabs) => {
+    
+    var numberOFTabs = tabs.length;
+
+    callback(numberOFTabs);
+  });
+}
+
+function renderStatus(statusText) {
+	document.getElementById('status').textContent = statusText;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  getNumberOfTabs( (numberOFTabs) => {
+  	var message = "You have " + numberOFTabs + " tabs opened in your current window.";
+
+  	renderStatus(message);
+  	});
+});
